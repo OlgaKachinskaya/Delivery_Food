354ab561edca70c58bb450787b6c9b3202f20e33
OlgaKachinskaya <leontieva-ov@mail.ru>
2021-12-06 12:59:31 +0300

    добавили media

--- js/main.js
+++ js/main.js
@@ -7,19 +7,6 @@ const btnClose = document.querySelector('.btn-close')
 const modalOpen = () => {
    modal.classList.toggle('modal--close')
 }
-
-//вызов функций//
-btnCart.addEventListener('click', modalOpen);
-btnClose.addEventListener('click', modalOpen)
-
-modal.addEventListener('click', (Event) => {
-    if (Event.target == modal) {
-        modalOpen()
-    }
-})
-
-//логгеры//
-
 const rows = modal.querySelectorAll('.row')
 const total = modal.querySelector('.modal-sum')
 
@@ -36,6 +23,20 @@ const getFullPrice = () => {
     
     total.textContent = fullPrice
 }
+//вызов функций//
+btnCart.addEventListener('click', modalOpen);
+btnClose.addEventListener('click', modalOpen);
+
+
+modal.addEventListener('click', (Event) => {
+    if (Event.target == modal) {
+        modalOpen()
+    }
+})
+
+//логгеры//
+
+
 
 rows.forEach(row => {
     let newPrice=0;
@@ -72,4 +73,28 @@ rows.forEach(row => {
 })
 getFullPrice()
  
+const mainIndex = document.getElementById('index')
+const mainRest = document.getElementById('restaurant')
+const cardsBlockIndex = mainIndex.querySelector('.cards')
+const cardsIndex = cardsBlockIndex.querySelectorAll('.card')
+const logos = document.querySelectorAll('.logo-link')
 
+
+const getToMain = () => {
+    mainRest.classList.toggle('not--active')
+    mainIndex.classList.toggle('not--active')
+}
+
+cardsIndex.forEach(card => {
+    card.addEventListener('click', () => {
+        getToMain()
+    })
+})
+logos.forEach(logo => {
+    logo.addEventListener('click', (event) => {
+        event.preventDefault();
+        if(mainIndex.classList.contains('not--active')) {
+            getToMain()
+        }
+    })
+})
